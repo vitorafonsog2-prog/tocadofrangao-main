7a41f599097055e61450d668332a7af95a3bc76d
vitorafonsog2-prog <vitorafonsog2@gmail.com>
2026-08-01 16:48:19 -0300

    Use local images instead of asset.json URLs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,20 +2,13 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Clock, Flame, Instagram, MapPin, Facebook } from "lucide-react";
 
 import { Reveal } from "../components/Reveal";
-import logoAsset from "../assets/logo.webp.asset.json";
+import logo from "../assets/logo.webp";
 import heroImg from "../assets/hero-frango.jpg";
-import historiaAsset from "../assets/historia.png.asset.json";
-import produtoAssadoAsset from "../assets/produto-assado.png.asset.json";
-import produtoChurrascoAsset from "../assets/produto-churrasco.png.asset.json";
-import produtoFritoAsset from "../assets/produto-frito.png.asset.json";
-import produtoCoxinhasAsset from "../assets/produto-coxinhas.png.asset.json";
-
-const logo = logoAsset.url;
-const historiaImg = historiaAsset.url;
-const produtoAssado = produtoAssadoAsset.url;
-const produtoChurrasco = produtoChurrascoAsset.url;
-const produtoFrito = produtoFritoAsset.url;
-const produtoCoxinhas = produtoCoxinhasAsset.url;
+import historiaImg from "../assets/historia.png";
+import produtoAssado from "../assets/produto-assado.png";
+import produtoChurrasco from "../assets/produto-churrasco.png";
+import produtoFrito from "../assets/produto-frito.png";
+import produtoCoxinhas from "../assets/produto-coxinhas.png";
 
 // TODO: substituir pelos links reais
 const LINK_IFOOD = "https://www.ifood.com.br/delivery/sao-joao-de-meriti-rj/toca-do-frangao-coelho-da-rocha/2d960de4-65fb-4f56-b1a3-6bbab25749dd?utm_medium=share";
